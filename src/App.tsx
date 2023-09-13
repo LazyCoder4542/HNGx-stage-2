54d7518a407799aa192cc82c9a77cae12cc2b312
Adeola Fadare <adeolafadare8@gmail.com>
2023-09-13 21:13:07 +0100

    fix: ts errors

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,6 @@ import Layout from "./components/Layout";
 import NetworkStatusIndicator from "./components/mini/NetworkStatusIndicator";
 
 function App() {
-  const url = 'https://api.themoviedb.org/3/authentication';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzlmNTVkNzYyYTAyYTEyMzBlNmM0Nzk0YTIxZWNmOCIsInN1YiI6IjYzZjIzOWVkYTY3MjU0MDA4NjVkYThmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.iEyFzMdwxT9vZIupxOSfSr8j7w1ow1-H-bBbixyva0U'
-    }
-  };
-  
-  // fetch(url, options)
-  //   .then(res => res.json())
-  //   .then(json => console.log(json))
-  //   .catch(err => console.error('error:' + err));
   return (
     <>
     <NetworkStatusIndicator/>
